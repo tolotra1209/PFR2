7970b421aae054a7743525c9acaec33b3adc7f2a
GuillaumeBASTIDE <133573221+GuillaumeBASTIDE@users.noreply.github.com>
2024-05-10 00:14:02 +0200

    Update commande_vocale_bluetooth.py

--- IHM/commande_vocale_bluetooth.py
+++ IHM/commande_vocale_bluetooth.py
@@ -6,7 +6,7 @@ with open('verbe.txt', 'r') as file:
     verbe_liste = file.read().splitlines()
 
 # Configurer la connexion Bluetooth
-bluetooth_port = '/dev/rfcomm0'  # mets le bon port
+bluetooth_port = 'COM8'  # mets le bon port
 baud_rate = 9600  
 bluetooth_conn = serial.Serial(bluetooth_port, baud_rate, timeout=1)
 
